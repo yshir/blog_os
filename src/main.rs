b326a9abda9fe4a99225559ab85d9074196190d3
yshir <yshir0020@gmail.com>
2021-11-21 18:12:47 +0900

    Make a test runner

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+use blog_os::println;
 use core::panic::PanicInfo;
 
-mod vga_buffer;
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
+#[allow(clippy::empty_loop)]
 #[allow(clippy::print_literal)]
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     println!("Hello, world{}", ".");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
