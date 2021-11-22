2e4dcb67398ef2b87f5aab5f43e56569963f130d
yshir <yshir0020@gmail.com>
2021-11-23 01:47:33 +0900

    Handle a double fault

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,6 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
-
     #[cfg(test)]
     test_main();
 
